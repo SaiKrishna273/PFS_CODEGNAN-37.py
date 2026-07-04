b07a14ff75f7b4ee91b544c08998e6f467d4cd1d
SaiKrishna273 <saikrishnagudla556@gmail.com>
2026-07-04 23:22:51 +0530

    Add files via upload
    # Python Execution: Compiler vs Interpreter
    
    This repository explains how Python code is executed internally by comparing the **Compiler** and **Interpreter** execution models. It covers Python's compilation to bytecode, the role of the **Python Virtual Machine (PVM)**, execution flow, and the key differences between compilation and interpretation using simple explanations and visual representations.
    
    ## Topics Covered
    
    * Compiler vs Interpreter
    * Python Code Execution Flow
    * Bytecode (`.pyc`)
    * Python Virtual Machine (PVM)
    * Runtime Execution
    * Execution Process Comparison
    * Beginner-Friendly Visual Diagrams
    
    This repository is part of my **Python Learning Journey** and is intended to help beginners understand how Python works behind the scenes.

---
+++ runtime.py
@@ -0,0 +1,5 @@
+#print(2+3)
+a = 5
+b = 7
+print(a+b)
+5
---
+++ runtimeinput.py
@@ -0,0 +1,39 @@
+'''a = 4
+b = 8
+print(a+b)''' 
+#run time input()
+'''a = int(input("enter the a value"))
+b = int(input("enter the b value"))
+print(a+b)
+a = float(input("enter the a float value"))
+b = float(input("enter the b float value"))
+print(a+b)'''
+#for string
+'''a = str(input("enter the a string value"))
+b = str(input("enter the b string value"))
+print(a+b)'''
+
+#for bool
+'''a = bool(input("enter the data1 value"))
+b = bool(input("enter the data2 value"))
+print(a+b)'''
+
+#for complex
+'''a = complex(input("enter the  complex a value"))
+b = complex(input("enter the complex b value"))'''
+
+
+a = int(input("a value"))
+b = int(input("b value"))
+option = int(input("choose the option 1.add 2.sub 3.mul"))
+print(a+b)
+print(a+b)
+print(a - b)
+print(a*b)
+a = int(input("a value"))
+b = int(input("b vlaue"))
+option = int('''choose option
+add
+sub'''
+)
+
