d2bf7067835e3073c1543dd439efd6a117eb4b27
SaiKrishna273 <saikrishnagudla556@gmail.com>
2026-07-08 22:19:45 +0530

    Add files via upload
    # Python Conditional Statements
    
    A collection of Python programs demonstrating conditional statements, including `if`, `if-else`, `if-elif-else`, nested `if`, multiple `if`, and the use of comparison, logical, membership, and identity operators through practical examples such as voting eligibility, leap year, login validation, and more.

---
+++ Tasks.py
@@ -0,0 +1,88 @@
+# for voting system
+'''age = int(input("Enter the the age:"))
+if age>=18:
+    print("eligible")
+else:
+    print("not eligible")'''
+
+# even or odd
+'''n = int(input("ENTER THE num:"))
+if n % 2 ==0:
+    print("even")
+else:
+    print("odd")'''
+#leap year
+'''n = int(input("enter the year:"))
+if n % 4 == 0:
+    print("leap year")
+elif n %400 == 0:
+
+else:
+    print("not leap year")'''
+#for leap year century
+'''year = int(input("enter the year"))
+    
+if year % 400 == 0 or (year % 4 == 0 and year % 100 != 0):
+    print("leap year")
+else:
+    print("not leap year")'''
+#for string
+'''name = str(input())
+if name == "saii":
+    print("welcome",name)
+else:
+    print("welcome guest")'''
+#for multiple names
+'''name = str(input())
+b = ["sai","krishna","venkata","harsha","trinadh"]
+if name in b:
+    print("welcome ",name)
+else:
+    print("welcome guest")'''
+
+#for vowels
+'''n = str(input()).lower()
+vowels = ["a","e","i","o","u"]
+if n in vowels:
+    print("vowel")
+else:
+    print("consonent")'''
+
+#social media
+'''us = str(input())
+pa = str(input())
+us1 = "saii"
+pa1 = 2004
+if us == "saii" :
+    if  pa ==2004:
+        print("login succesfull")
+    else:
+     print("invalid credentials")
+else:
+    print("not matched")'''
+'''username = "saii"
+password = "2004"
+
+user = input("Enter the username: ")
+pas = input("Enter the password: ")
+
+if user == username and pas == password:
+    print("Login successful")
+else:
+    print("Not valid")'''
+
+age = int(input())
+marks = int(input())
+attendance = int(input())
+if age >=18:
+   print("eligible for scholar")
+if marks >= 80:
+       print("eligible for marks")
+if attendance >=70:
+           print("eligible for attendance")
+        
+
+    
+
+
+
