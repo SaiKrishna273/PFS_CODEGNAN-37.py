6ef7941557ac8fb78ec1247370bbbab665156aca
SaiKrishna273 <saikrishnagudla556@gmail.com>
2026-07-11 08:30:57 +0530

    Add files via upload
    The code will execute if it was true

---
+++ while_loop.py
@@ -0,0 +1,121 @@
+#while loop()
+#infinite execution
+'''a = 10
+while a>1:
+    print(a)
+
+a = 10
+while a<1:
+    print(a)
+
+a= 10
+while a>1:
+    print(a)
+    a = a-1
+
+
+a =10
+while a>1:
+    print(a)
+    a = a-1
+
+a =10
+while a>=1:
+    print(a)
+    a = a-1
+
+a =20
+while a>3:
+    print(a)
+    a = a-1
+
+a =20
+while a>3:
+    a =a-1
+print(a)
+
+
+a =40
+while a>5:
+    a=a-1
+print(a)
+
+a =30
+while a>1:
+    print(a)
+    a+=1
+
+a =10
+while a>2:
+    print(a)
+    a-=1
+
+a=30
+while a>1:
+    print(a)
+    a-=1
+
+
+a =1
+while a<25:
+    print(a)
+    a+=1'''
+
+#range()-function
+#Range function returns a sequence of a number starts with 0 by default and increments by 1 by 1 and stops before a specified number
+
+#start-stop-step
+'''for i in range(20):
+    print(i)'''
+'''for i in range(13,35):
+    print(i)'''
+'''for i in range(0,28,3):
+    print(i)
+
+for i in range(5,46,5):
+    print(i)
+for i in range(2,19,2):
+    print(i)'''
+
+'''for i in range(100,0,-5):
+    print(i)'''
+
+'''for i in range(0,30,3):
+    print(i)
+
+for i in range(0,50,5):
+    print(i)
+
+for i in range(0,20,2):
+    print(i)'''
+
+
+#Task - on all concepts
+'''while True:
+    marks = int(input("enter the marks:"))
+    if marks in range(91,101):
+         print("Grade A")
+    elif marks in range(81,91):
+        print("Grade B")
+    elif marks in range(70,81):
+        print("Grade C")
+    elif marks in range(50,71):
+        print("Grade D")
+    else:
+        print("Fail")'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
